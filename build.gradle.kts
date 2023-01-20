050250fb856afb00f6a7d86490b37f8a4b6630c7
DatLag <jeff.retz08@gmail.com>
2023-01-20 17:48:33 +0100

    added multiplatform sourcesets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,31 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("multiplatform") version "1.7.20"
     id("org.jetbrains.compose") version "1.2.2"
+    id("maven-publish")
+    id("signing")
+    id("com.vanniktech.maven.publish") version "0.22.0"
 }
 
 group = "dev.datlag.shapeofcomposable"
 version = "1.0.0"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
+    macosX64()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    jvm()
+    js(IR) {
+        browser()
+        binaries.executable()
     }
     sourceSets {
         val commonMain by getting {
@@ -23,18 +33,46 @@ kotlin {
                 implementation(compose.foundation)
             }
         }
-        val jvmMain by getting {
-            dependencies {
-                api(compose.runtime)
-                api(compose.foundation)
-                implementation(compose.desktop.currentOs)
-            }
+        val jvmMain by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
 
-compose.desktop {
-    application {
-        mainClass = "dev.datlag.shapeofcomposable.MainKt"
+mavenPublishing {
+    publishToMavenCentral(host = SonatypeHost.S01, automaticRelease = true)
+    signAllPublications()
+
+    pom {
+        name.set(project.name)
+        description.set("ShapeOfComposable is a multi-platform compose library to clip any composeable to a custom shape.")
+        url.set("https://github.com/DatL4g/ShapeOfComposable")
+
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/DatL4g/ShapeOfComposable")
+            connection.set("scm:git:git://github.com/DatL4g/ShapeOfComposable.git")
+        }
+
+        developers {
+            developer {
+                id.set("DatL4g")
+                name.set("Jeff Retz (DatLag)")
+                url.set("https://github.com/DatL4g")
+            }
+        }
     }
 }
